3033f90bac1ba0c78fb0e4bcb70abc05ae6ce083
Brendon Butler <bbutler452@gmail.com>
2021-10-04 23:23:59 -0400

    Create loop, clean up, add notes

--- binobucks.asm
+++ binobucks.asm
@@ -22,47 +22,39 @@ main: 	.macro print_int
 	syscall
 	.end_macro
 	
-	.macro check (%power)
-	sll	$t1, $t0, %power 
-	div 	$a0, $a0, $t1
-	mflo	$a0
-	print_int
+	.macro check ($power)
+	sllv	$t1, $t0, $power	# shift left by the power given, store value in $t1
+	div 	$a0, $a0, $t1		# divide $a0 (quotient or original number) by $t1
+	mflo	$a0			# move quotient to $a0
+	print_int			# print integer in $a0
 	
-	li $a0, 32 	# print whitepace as line separator
-	li $v0, 11	# syscall for char print
+	li 	$a0, 32 		# print whitepace as line separator
+	li 	$v0, 11			# syscall for char print
 	syscall
 	
-	la	$a0, ($t1)
+	la	$a0, ($t1)		# print dollar bill denomination
 	print_int
-	la	$a0, dollar
+	la	$a0, dollar		# print dollar text
 	print_str
-	mfhi	$a0
+	mfhi	$a0			# move remainder into $a0
 	.end_macro
 	
 # START OF PROGRAM
 	li	$t0, 1		# set $t0 to 1 for powers
-
 	la 	$a0, intro	# load the label value into $a0
+	print_str	
+	la 	$a0, prompt	# prompt user for number between 1-2048
 	print_str
-	
-	la 	$a0, prompt
-	print_str
-	
 	la 	$v0, 5		# get user input
 	syscall
-	
 	move	$a0, $v0	# store user input in $a0
 	
-	check (10)
-	check (9)
-	check (8)
-	check (7)
-	check (6)
-	check (5)
-	check (4)
-	check (3)
-	check (2)
-	check (1)
-	check (0)
+	li	$t2, 10 	# power to start (10 = 1024, 9 = 512, ...)
+	
+# TODO : PRINT result banner with user input value embedded
+loop:
+	check ($t2)
+	sub $t2, $t2, 1
+	bne $t2, -1, loop
 	
 	done
